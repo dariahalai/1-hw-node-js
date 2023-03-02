35ba2a6037617de297ded1872085e28c9301c5b2
Daria Halai <106152100+dariahalai@users.noreply.github.com>
2023-03-03 01:22:21 +0300

    CLI
    create CLI operations with Contacts

---
+++ README.md
@@ -0,0 +1,18 @@
+
+# First HW Node.js
+
+ Отримуємо і виводимо весь список контактів у вигляді таблиці 
+
+https://monosnap.com/file/0fsIh5Rkwy3LQpXYeayJKcVnYoal7T
+
+ Отримуємо контакт по id
+
+https://monosnap.com/file/NeOCXBHiaLLdZt43TTKjZuKwbzfdFc
+
+ Додаємо контакт
+ 
+https://monosnap.com/file/r7XQAlHAKhEdaRzkt7mUH2iE8b0w7H
+
+ Видаляємо контакт
+
+https://monosnap.com/file/cos6xYjpug9a8ybDzm2gfjdFFjCb4Q
---
+++ contacts.js
@@ -0,0 +1,62 @@
+const path = require("path");
+const fs = require("fs").promises;
+
+const contactsPath = path.join("db", "contacts.json");
+
+const getParseContactsPath = async (path) => {
+  const data = await fs.readFile(path);
+  return JSON.parse(data);
+};
+
+const listContacts = async () => {
+  try {
+    const parseData = getParseContactsPath(contactsPath);
+    console.table(parseData);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const getContactById = async (contactId) => {
+  try {
+    const parseData = getParseContactsPath(contactsPath);
+    const findContact = parseData.find((item) => item.id === contactId);
+    console.table(findContact);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const removeContact = async (contactId) => {
+  try {
+    const parseData = getParseContactsPath(contactsPath);
+    const deleteContact = parseData.filter((item) => item.id !== contactId);
+    console.table(deleteContact);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const addContact = async (name, email, phone) => {
+  try {
+    const parseData = getParseContactsPath(contactsPath);
+    const newContact = {
+      id: `${parseData.length + 1}`,
+      name,
+      email,
+      phone,
+    };
+    const data = [...parseData, newContact];
+    console.table(data);
+    return await fs.writeFile(contactsPath, JSON.stringify(data));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+module.exports = {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+};
